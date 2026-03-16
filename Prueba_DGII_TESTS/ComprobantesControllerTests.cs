40e03e96fb31dfbfc4f5bbc84c54f918dbb4c8e4
Darwin Castillo <castillomaderadarwin@gmail.com>
2026-03-16 19:29:26 -0400

    Adding all unit tests to the project

---
+++ Prueba_DGII_TESTS/ComprobantesControllerTests.cs
@@ -0,0 +1,136 @@
+﻿
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Prueba_DGII.Controllers;
+using Prueba_DGII.DTOs;
+using Prueba_DGII.Models;
+
+namespace Prueba_DGII_TESTS
+{
+    public class ComprobantesControllerTests
+    {
+
+        [Fact]
+        public async Task GetComprobantes_ReturnsList()
+        {
+            var context = TestDbContextFactory.Create();
+
+            var contribuyente = new Contribuyente
+            {
+                RncCedula = "123",
+                Nombre = "Empresa Test",
+                Tipo = "PERSONA JURIDICA",
+                Estatus = "activo"
+            };
+
+            context.Contribuyentes.Add(contribuyente);
+
+            context.ComprobantesFiscales.Add(new ComprobanteFiscal
+            {
+                RncCedula = "123",
+                NCF = "E310000000001",
+                Monto = 200,
+                Itbis18 = 36
+            });
+
+            await context.SaveChangesAsync();
+
+            var controller = new ComprobantesController(context);
+
+            var result = await controller.GetComprobantes();
+
+            result.Should().BeOfType<OkObjectResult>();
+        }
+
+
+        [Fact]
+        public async Task CrearComprobante_ReturnsBadRequest_WhenContribuyenteNoExiste()
+        {
+            var context = TestDbContextFactory.Create();
+
+            var controller = new ComprobantesController(context);
+
+            var dto = new CrearComprobanteDto
+            {
+                RncCedula = "999",
+                NCF = "E310000000002",
+                Monto = 100
+            };
+
+            var result = await controller.CrearComprobante(dto);
+
+            result.Should().BeOfType<BadRequestObjectResult>();
+        }
+
+
+        [Fact]
+        public async Task CrearComprobante_CreatesComprobante()
+        {
+            var context = TestDbContextFactory.Create();
+
+            context.Contribuyentes.Add(new Contribuyente
+            {
+                RncCedula = "123",
+                Nombre = "Empresa Test",
+                Tipo = "PERSONA JURIDICA",
+                Estatus = "activo"
+            });
+
+            await context.SaveChangesAsync();
+
+            var controller = new ComprobantesController(context);
+
+            var dto = new CrearComprobanteDto
+            {
+                RncCedula = "123",
+                NCF = "E310000000010",
+                Monto = 100
+            };
+
+            var result = await controller.CrearComprobante(dto);
+
+            result.Should().BeOfType<OkObjectResult>();
+
+            context.ComprobantesFiscales.Count().Should().Be(1);
+        }
+
+
+        [Fact]
+        public async Task CrearComprobante_ReturnsBadRequest_WhenNCFDuplicado()
+        {
+            var context = TestDbContextFactory.Create();
+
+            context.Contribuyentes.Add(new Contribuyente
+            {
+                RncCedula = "123",
+                Nombre = "Empresa Test",
+                Tipo = "PERSONA JURIDICA",
+                Estatus = "activo"
+            });
+
+            context.ComprobantesFiscales.Add(new ComprobanteFiscal
+            {
+                RncCedula = "123",
+                NCF = "E310000000001",
+                Monto = 200,
+                Itbis18 = 36
+            });
+
+            await context.SaveChangesAsync();
+
+            var controller = new ComprobantesController(context);
+
+            var dto = new CrearComprobanteDto
+            {
+                RncCedula = "123",
+                NCF = "E310000000001",
+                Monto = 500
+            };
+
+            var result = await controller.CrearComprobante(dto);
+
+            result.Should().BeOfType<BadRequestObjectResult>();
+        }
+
+    }
+}
---
+++ Prueba_DGII_TESTS/ContribuyentesControllerTests.cs
@@ -0,0 +1,103 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Prueba_DGII.Controllers;
+using Prueba_DGII.DTOs;
+using Prueba_DGII.Models;
+
+
+namespace Prueba_DGII_TESTS
+{
+    public class ContribuyentesControllerTests
+    {
+
+        [Fact]
+        public async Task GetContribuyentes_ReturnsList()
+        {
+            var context = TestDbContextFactory.Create();
+
+            context.Contribuyentes.Add(new Contribuyente
+            {
+                RncCedula = "123",
+                Nombre = "Empresa Test",
+                Tipo = "PERSONA JURIDICA",
+                Estatus = "activo"
+            });
+
+            await context.SaveChangesAsync();
+
+            var controller = new ContribuyentesController(context);
+
+            var result = await controller.GetContribuyentes();
+
+            result.Should().BeOfType<OkObjectResult>();
+        }
+
+
+        [Fact]
+        public async Task GetComprobantesPorContribuyente_ReturnsNotFound_WhenNotExists()
+        {
+            var context = TestDbContextFactory.Create();
+
+            var controller = new ContribuyentesController(context);
+
+            var result = await controller.GetComprobantesPorContribuyente("999");
+
+            result.Should().BeOfType<NotFoundObjectResult>();
+        }
+
+
+        [Fact]
+        public async Task CrearContribuyente_CreatesNewContribuyente()
+        {
+            var context = TestDbContextFactory.Create();
+
+            var controller = new ContribuyentesController(context);
+
+            var dto = new CrearContribuyenteDto
+            {
+                RncCedula = "987654321",
+                Nombre = "Empresa Nueva",
+                Tipo = "PERSONA JURIDICA",
+                Estatus = "activo"
+            };
+
+            var result = await controller.CrearContribuyente(dto);
+
+            result.Should().BeOfType<OkObjectResult>();
+
+            context.Contribuyentes.Count().Should().Be(1);
+        }
+
+
+        [Fact]
+        public async Task CrearContribuyente_ReturnsBadRequest_WhenRncDuplicado()
+        {
+            var context = TestDbContextFactory.Create();
+
+            context.Contribuyentes.Add(new Contribuyente
+            {
+                RncCedula = "123",
+                Nombre = "Empresa Test",
+                Tipo = "PERSONA JURIDICA",
+                Estatus = "activo"
+            });
+
+            await context.SaveChangesAsync();
+
+            var controller = new ContribuyentesController(context);
+
+            var dto = new CrearContribuyenteDto
+            {
+                RncCedula = "123",
+                Nombre = "Empresa Duplicada",
+                Tipo = "PERSONA JURIDICA",
+                Estatus = "activo"
+            };
+
+            var result = await controller.CrearContribuyente(dto);
+
+            result.Should().BeOfType<BadRequestObjectResult>();
+        }
+
+    }
+}
